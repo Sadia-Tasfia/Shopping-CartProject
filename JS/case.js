ca9af28778916989a24c40d5fd875780beac633e
Sadia-Tasfia <tasfia.sujana2903@gmail.com>
2023-07-21 01:38:27 +0600

    second commit

--- JS/case.js
+++ JS/case.js
@@ -5,22 +5,61 @@
 4. calculate the new number
 5. set the value to the number field
 */ 
- 
-document.getElementById("btn-case-plus").addEventListener('click', function(){
-  const numberField = document.getElementById("number-field");
-  const numberString = numberField.value;
-  const previousNumber = parseInt(numberString); 
 
-  const newNumber = previousNumber + 1;
-  numberField.value = newNumber;
-});
+// document.getElementById("btn-case-plus").addEventListener('click', function(){
+//   const numberField = document.getElementById("number-field");
+//   const numberString = numberField.value;
+//   const previousNumber = parseInt(numberString); 
 
-document.getElementById("btn-case-minus").addEventListener('click', function(){
+//   const newNumber = previousNumber + 1;
+//   numberField.value = newNumber;
+// });
+
+// document.getElementById("btn-case-minus").addEventListener('click', function(){
+//   const numberField = document.getElementById("number-field");
+//   const numberString = numberField.value;
+//   const previousNumber = parseInt(numberString);
+  
+//   const newNumber = previousNumber - 1;
+//   numberField.value = newNumber
+// });
+
+function updateCaseNumber(isIncrease) {
   const numberField = document.getElementById("number-field");
   const numberString = numberField.value;
   const previousNumber = parseInt(numberString);
+
+  let newNumber;
+  if (isIncrease) {
+    newNumber = previousNumber + 1;
+  } else {
+    newNumber = previousNumber - 1;
+  }
+  numberField.value = newNumber;
+  return newNumber;
+}
+
+function updateCaseTotalPrice (newNumber) {
+  const caseTotalPrice = newNumber * 55;
+
+  const caseTotalElement = document.getElementById("case-total");
+  caseTotalElement.innerText = caseTotalPrice;
+}
+
+document.getElementById("btn-case-plus").addEventListener('click', function(){
+  // updateCaseNumber(true);
+  const newNumber= updateCaseNumber(true);
+  updateCaseTotalPrice(newNumber);
   
-  const newNumber = previousNumber - 1;
-  numberField.value = newNumber
-  
+});
+
+document.getElementById("btn-case-minus").addEventListener('click', function(){
+  /* 
+  const caseTotalPrice = newNumber * 55;
+  const caseTotalElement = document.getElementById("case-total");
+  caseTotalElement.innerText = caseTotalPrice;
+  */
+
+  const newNumber = updateCaseNumber(false);
+  updateCaseTotalPrice(newNumber)
 });
---
+++ JS/phone.js
@@ -0,0 +1,31 @@
+function updatePhoneNumber(isIncrease) {
+  const phoneField = document.getElementById("phone-field");
+  const phoneFieldString = phoneField.value;
+  const previousPhone = parseInt(phoneFieldString);
+
+  let newPhone;
+
+  if (isIncrease){
+    newPhone = previousPhone +1;
+  } else{
+    newPhone = previousPhone - 1;
+  }
+  phoneField.value = newPhone;
+
+  return newPhone;
+}
+
+function updatePhoneTotal(newPhone){
+  const phoneTotalPrice = newPhone * 1219;
+  const phoneTotalElement = document.getElementById("phone-total");
+  phoneTotalElement.innerText = phoneTotalPrice;
+}
+document.getElementById("btn-phone-plus").addEventListener('click', function(){
+  const newPhone = updatePhoneNumber(true);
+  updatePhoneTotal(newPhone);
+
+});
+
+document.getElementById("btn-phone-minus").addEventListener('click', function(){
+  updatePhoneNumber(false);
+});
